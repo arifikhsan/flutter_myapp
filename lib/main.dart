e88ea756f4f1507817edc9ec4eaa8db0c8ec171d
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-11-27 21:24:36 +0700

    icon and button

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,42 @@ class Home extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
-            Text(
-              'hello ninjas',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2,
-                  color: Colors.grey[600],
-                  fontFamily: 'IndieFlower'),
+            Icon(
+              Icons.airport_shuttle,
+              size: 100,
+              color: Colors.lightBlue,
             ),
-            Image(
-              image: NetworkImage('https://via.placeholder.com/125'),
+            RaisedButton(
+              onPressed: () {
+                print('you clicked me');
+              },
+              child: Text(
+                "Click me",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.lightGreen,
             ),
-            Image(
-              image: AssetImage('assets/200.png'),
+            FlatButton(
+              onPressed: () {},
+              child: Text('flat'),
+              color: Colors.blue,
             ),
-            Image.asset('assets/100.png'),
-            Image.network('https://via.placeholder.com/50')
+            RaisedButton.icon(
+              onPressed: () {},
+              icon: Icon(
+                Icons.mail,
+                color: Colors.white,
+              ),
+              label: Text('Mail me'),
+              color: Colors.amber,
+            ),
+            IconButton(
+              icon: Icon(Icons.alternate_email),
+              onPressed: () {
+                print('@ you click @');
+              },
+              color: Colors.amber,
+            )
           ],
         ),
       ),
