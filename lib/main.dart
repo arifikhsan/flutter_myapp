d52e4719be962d9c428b587ebb7ba50e82853e01
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-11-27 11:46:16 +0700

    center and floating button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Text('sdfsfd'),
-    ));
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('My First App'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Text('hello ninjas'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Text('Click'),
+            onPressed: null,
+          ),
+        ),
+      ),
+    );
