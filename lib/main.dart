cd435a255f5bef6d5761b65ef77ace7389cb2381
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-11-27 19:05:29 +0700

    enable hot reload functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-            centerTitle: true,
-            backgroundColor: Colors.red[600],
-          ),
-          body: Center(
-            child: Text(
-              'hello ninjas',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2,
-                color: Colors.grey[600],
-                fontFamily: 'IndieFlower'
-              ),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Text('Click'),
-            onPressed: () {},
-            backgroundColor: Colors.red[600],
-          ),
+      MaterialApp(home: Home()),
+    );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'hello ninjas enough action',
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
     );
+  }
+}
